0c39ee11234c8087053b4a206b5fa8f5fd5c83d0
Allison Howard <alliemhow@gmail.com>
2019-05-20 18:02:34 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -23,8 +23,9 @@ class ApplicationController < Sinatra::Base
     erb :edit
   end
 
-  post '/recipes/new' do
+  post '/recipes' do
     @recipe = Recipe.create(:name => params[:name], :ingredients => params[:ingredients], :cook_time => params[:cook_time])
+    @recipe.save
     redirect to "/recipes/#{@recipe.id}"
   end
 
